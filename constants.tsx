bdb4ae5d9101fbfadd2e853a09163879fce43515
hamiltonmatematica <hamilton.vinicius@gmail.com>
2026-03-10 16:31:37 -0300

    update: datas Rio Pardo de Minas e ordenação

--- constants.tsx
+++ constants.tsx
@@ -89,10 +89,10 @@ export const AGENDA_DAY_2: AgendaItem[] = [
 
 export const CITIES: City[] = [
   { name: "Montes Claros", date: "09 de Fevereiro" },
-  { name: "Brasília de Minas", date: "A definir" },
-  { name: "Rio Pardo de Minas", date: "16 e 17 de Março" },
-  { name: "Taiobeiras", date: "18 e 19 de Março" },
+  { name: "Taiobeiras", date: "18 e 19 de março" },
   { name: "Bocaiuva", date: "26 e 27 de Março" },
+  { name: "Rio Pardo de Minas", date: "23 e 24 de Abril" },
+  { name: "Brasília de Minas", date: "A definir" },
   { name: "Coração de Jesus", date: "Data a confirmar" },
   { name: "Corinto", date: "Data a confirmar" },
   { name: "Guanambi", date: "Data a confirmar" }
@@ -100,10 +100,10 @@ export const CITIES: City[] = [
 
 export const CITIES_FULL: City[] = [
   { name: "Montes Claros", date: "09 e 10 de Fevereiro" },
-  { name: "Brasília de Minas", date: "A definir" },
-  { name: "Rio Pardo de Minas", date: "16 e 17 de Março" },
-  { name: "Taiobeiras", date: "18 e 19 de Março" },
+  { name: "Taiobeiras", date: "18 e 19 de março" },
   { name: "Bocaiuva", date: "26 e 27 de Março" },
+  { name: "Rio Pardo de Minas", date: "23 e 24 de Abril" },
+  { name: "Brasília de Minas", date: "A definir" },
   { name: "Coração de Jesus", date: "Data a confirmar" },
   { name: "Corinto", date: "Data a confirmar" },
   { name: "Guanambi", date: "Data a confirmar" }
